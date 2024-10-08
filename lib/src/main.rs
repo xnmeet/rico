e9dd53b24e765ddb167e68bf5421719c2fb5aaf0
xnmeet <xxnmeet@gmail.com>
2024-10-08 15:21:15 +0800

    feat: Add peek method, adjust advance implementation, retain copies of two tokens

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use thrift_parser::parser::Parser; // Add this line to import the fs module
 
 fn main() {
-    let input = fs::read_to_string("tests/header.thrift").expect("Unable to read file"); // Handle potential errors
+    let input = fs::read_to_string("tests/typedef.thrift").expect("Unable to read file"); // Handle potential errors
 
     let mut temp = Parser::new(&input);
     let result = temp.parse();
--- lib/src/parser/mod.rs
+++ lib/src/parser/mod.rs
@@ -7,9 +7,18 @@ use crate::parser::error::ParseError;
 use factory::*;
 use logos::Logos;
 
+#[derive(Debug, Clone)]
+pub struct ParserToken<'a> {
+    pub text: &'a str,
+    pub span: logos::Span,
+    pub token: Token,
+    pub extras: (usize, usize),
+}
+
 pub struct Parser<'a> {
     lexer: logos::Lexer<'a, Token>,
-    current_token: Option<Token>,
+    cur_token: Option<ParserToken<'a>>,
+    next_token: Option<ParserToken<'a>>,
 }
 
 impl<'a> Parser<'a> {
@@ -17,7 +26,8 @@ impl<'a> Parser<'a> {
         let lexer = Token::lexer(input);
         Parser {
             lexer,
-            current_token: None,
+            next_token: None,
+            cur_token: None,
         }
     }
 
@@ -27,7 +37,7 @@ impl<'a> Parser<'a> {
         loop {
             self.advance();
             self.skip_comments();
-            if let Some(token) = &self.current_token {
+            if let Some(token) = self.token() {
                 match token {
                     Token::Include => {
                         members.push(DocumentMembers::Include(self.parse_include()?));
@@ -37,6 +47,7 @@ impl<'a> Parser<'a> {
                     }
                     Token::Const => members.push(DocumentMembers::Const(self.parse_const()?)),
                     Token::Typedef => members.push(DocumentMembers::Typedef(self.parse_typedef()?)),
+                    // Token::Enum => members.push(DocumentMembers::Enum(self.parse_enum()?)),
                     _ => {
                         return Err(ParseError::UnexpectedToken(self.start_pos()));
                     }
@@ -56,61 +67,130 @@ impl<'a> Parser<'a> {
         // 处理换行字符串
         self.bump_block_token_loc();
 
-        let current_token = self.lexer.next();
-        if let Some(Ok(token)) = current_token {
-            self.current_token = Some(token);
-        } else {
-            self.current_token = None;
+        // 如果没有下一个 token 且当前 token 存在，清空当前 token
+        if self.next_token.is_none() && self.cur_token.is_some() {
+            self.cur_token = None;
+            return None;
         }
-        return self.current_token.as_ref();
+
+        // 更新当前 token
+        if let Some(ref mut token) = self.next_token {
+            if let Some(ref cur) = self.cur_token {
+                token.extras.0 = cur.extras.0.max(token.extras.0);
+            }
+            self.cur_token = self.next_token.take(); // Move next_token to cur_token
+        }
+
+        // 获取当前 token
+        if self.cur_token.is_none() {
+            if let Some(Ok(token)) = self.lexer.next() {
+                self.cur_token = Some(ParserToken {
+                    text: self.lexer.slice(),
+                    span: self.lexer.span(),
+                    token,
+                    extras: self.lexer.extras,
+                });
+            }
+        }
+
+        // 获取下一个 token
+        self.next_token = self
+            .lexer
+            .next()
+            .map(|result| {
+                result.map(|token| ParserToken {
+                    text: self.lexer.slice(),
+                    span: self.lexer.span(),
+                    token,
+                    extras: self.lexer.extras,
+                })
+            })
+            .transpose()
+            .unwrap_or(None);
+
+        self.cur_token.as_ref().map(|token| &token.token)
+    }
+
+    fn peek(&self) -> Option<&Token> {
+        if let Some(token) = &self.next_token {
+            return Some(&token.token);
+        }
+
+        return None;
+    }
+
+    fn token(&self) -> Option<&Token> {
+        if let Some(token) = &self.cur_token {
+            return Some(&token.token);
+        }
+
+        return None;
+    }
+
+    fn text(&self) -> &str {
+        if let Some(token) = &self.cur_token {
+            return token.text;
+        }
+        return "";
     }
 
     fn bump_block_token_loc(&mut self) {
-        if let Some(token) = &self.current_token {
+        if let Some(token) = self.token() {
             if token == &Token::StringLiteral || token == &Token::BlockComment {
-                let count = self.lexer.slice().matches('\n').count();
+                let count = self.text().matches('\n').count();
                 if count > 0 {
-                    self.lexer.extras.0 += count;
-                    self.lexer.extras.1 = self.lexer.span().end;
+                    if let Some(ref mut token) = self.cur_token {
+                        token.extras.0 += count;
+                        token.extras.1 = self.lexer.span().end;
+                    }
                 }
             }
         }
     }
 
     fn start_pos(&self) -> Span {
-        let line = self.lexer.extras.0 + 1;
+        let mut line = 0;
         // The addition of 1 here is on the one hand to make the column start at 1
         // and on the other hand to avoid newline value overflow
-        let column = self.lexer.span().start + 1 - self.lexer.extras.1;
+        let mut column = 0;
+        let mut index = 0;
+        if let Some(token) = &self.cur_token {
+            line = token.extras.0 + 1;
+            column = token.span.start + 1 - token.extras.1;
+            index = token.span.start
+        }
 
         Span {
             line,
             column,
-            index: self.lexer.span().start,
+            index,
         }
     }
 
     fn end_pos(&self) -> Span {
-        if let Some(token) = &self.current_token {
+        let (extras_0, extras_1) = self.cur_token.as_ref().map_or((0, 0), |t| t.extras);
+        let span_end = self.cur_token.as_ref().map_or(0, |t| t.span.end);
+
+        if let Some(token) = self.token() {
             if token == &Token::StringLiteral || token == &Token::BlockComment {
                 // Handling newline strings
-                let split_vec: Vec<&str> = self.lexer.slice().split('\n').collect();
+                let split_vec: Vec<&str> = self.text().split('\n').collect();
                 if split_vec.len() > 1 {
                     return Span {
-                        line: self.lexer.extras.0 + split_vec.len(),
+                        line: extras_0 + split_vec.len(),
                         column: split_vec.last().unwrap().len() + 1,
-                        index: self.lexer.span().end,
+                        index: span_end,
                     };
                 }
             }
         }
 
-        let line = self.lexer.extras.0 + 1;
-        let column = self.lexer.span().end + 1 - self.lexer.extras.1;
+        let line = extras_0 + 1;
+        let column = span_end + 1 - extras_1;
         Span {
             line,
             column,
-            index: self.lexer.span().end,
+            index: span_end,
         }
     }
 
@@ -130,7 +210,7 @@ impl<'a> Parser<'a> {
 
         self.consume(Token::StringLiteral)?;
         let value_loc = self.get_token_loc();
-        let value = self.lexer.slice();
+        let value = self.text();
 
         Ok(Include {
             name: Common {
@@ -150,12 +230,12 @@ impl<'a> Parser<'a> {
         let scope_loc = self.get_token_loc();
 
         // for example: namespace go a.b.c
-        let indent_scope = self.lexer.slice(); // result go
+        let indent_scope = self.text().to_owned(); // result go
 
         self.consume(Token::Identifier)?;
         let value_loc = self.get_token_loc();
 
-        let value = self.lexer.slice(); // result a.b.c
+        let value = self.text(); // result a.b.c
 
         Ok(Namespace {
             kind: NodeType::NamespaceDefinition,
@@ -179,12 +259,11 @@ impl<'a> Parser<'a> {
 
         self.consume(Token::Identifier)?;
         let name_loc = self.get_token_loc();
-        let name = self.lexer.slice();
+        let name = self.text().to_owned();
 
         self.consume(Token::Equals)?;
 
         let const_value = self.parse_field_value()?;
-
         Ok(Const {
             kind: NodeType::ConstDefinition,
             loc: self.get_token_parent_loc(const_start_pos, self.get_token_loc().end),
@@ -204,7 +283,7 @@ impl<'a> Parser<'a> {
 
         self.consume(Token::Identifier)?;
         let name_loc = self.get_token_loc();
-        let name = self.lexer.slice();
+        let name = self.text();
 
         Ok(Typedef {
             kind: NodeType::TypedefDefinition,
@@ -218,11 +297,11 @@ impl<'a> Parser<'a> {
         })
     }
 
-    fn is_list_token_end(&mut self) -> bool {
+    fn is_bracket_end(&mut self) -> bool {
         self.expect_token(Token::RightBracket).is_ok()
     }
 
-    fn is_map_token_end(&mut self) -> bool {
+    fn is_brace_end(&mut self) -> bool {
         self.expect_token(Token::RightBrace).is_ok()
     }
 
@@ -231,7 +310,7 @@ impl<'a> Parser<'a> {
         F: Fn(LOC, &str, FieldType) -> FieldType,
     {
         let start_loc = self.start_pos();
-        let slice = self.lexer.slice();
+        let slice = self.text().to_owned();
 
         self.consume(Token::LeftAngle)?;
         let filed_type = self.parse_field_type()?;
@@ -243,7 +322,7 @@ impl<'a> Parser<'a> {
                 start: start_loc,
                 end: end_loc.end,
             },
-            slice,
+            &slice,
             filed_type,
         ))
     }
@@ -258,7 +337,7 @@ impl<'a> Parser<'a> {
 
     fn parse_map_type(&mut self) -> Result<FieldType, ParseError> {
         let start_loc = self.start_pos();
-        let slice = self.lexer.slice();
+        let slice = self.text().to_owned();
 
         self.consume(Token::LeftAngle)?;
         let filed_key_type = self.parse_field_type()?;
@@ -271,7 +350,7 @@ impl<'a> Parser<'a> {
         let end_loc = self.get_token_loc();
         Ok(create_map_field_type(
             self.get_token_parent_loc(start_loc, end_loc.end),
-            slice,
+            &slice,
             filed_key_type,
             filed_value_type,
         ))
@@ -279,7 +358,7 @@ impl<'a> Parser<'a> {
 
     fn parse_field_type(&mut self) -> Result<FieldType, ParseError> {
         self.advance();
-        match &self.current_token {
+        match self.token() {
             Some(token) => match token {
                 Token::Binary
                 | Token::String
@@ -291,11 +370,11 @@ impl<'a> Parser<'a> {
                 | Token::Bool => Ok(create_keyword_field_type(
                     token,
                     self.get_token_loc(),
-                    self.lexer.slice(),
+                    self.text(),
                 )),
                 Token::Identifier => Ok(create_identifier_field_type(
                     self.get_token_loc(),
-                    self.lexer.slice(),
+                    self.text(),
                 )),
                 Token::List => self.parse_list_type(),
                 Token::Map => self.parse_map_type(),
@@ -315,14 +394,14 @@ impl<'a> Parser<'a> {
 
             if let Ok(val) = value {
                 elements.push(val);
-            } else if self.is_list_token_end() {
+            } else if self.is_bracket_end() {
                 break;
             } else {
                 return value;
             }
 
             self.advance();
-            if self.is_list_token_end() {
+            if self.is_bracket_end() {
                 break;
             }
             self.expect_token(Token::Comma)?;
@@ -352,14 +431,14 @@ impl<'a> Parser<'a> {
                     name: property_key,
                     value,
                 });
-            } else if self.is_map_token_end() {
+            } else if self.is_brace_end() {
                 break;
             } else {
                 return property_value;
             }
 
             self.advance();
-            if self.is_map_token_end() {
+            if self.is_brace_end() {
                 break;
             }
             self.expect_token(Token::Comma)?;
@@ -373,20 +452,15 @@ impl<'a> Parser<'a> {
 
     fn parse_field_value(&mut self) -> Result<FieldInitialValue, ParseError> {
         self.advance();
-        match &self.current_token {
+        match self.token() {
             Some(token) => match token {
                 Token::StringLiteral
                 | Token::IntegerLiteral
                 | Token::DoubleLiteral
-                | Token::BooleanLiteral => Ok(create_const_value(
-                    token,
-                    self.get_token_loc(),
-                    self.lexer.slice(),
-                )),
-                Token::Identifier => Ok(create_identifier_value(
-                    self.get_token_loc(),
-                    self.lexer.slice(),
-                )),
+                | Token::BooleanLiteral => {
+                    Ok(create_const_value(token, self.get_token_loc(), self.text()))
+                }
+                Token::Identifier => Ok(create_identifier_value(self.get_token_loc(), self.text())),
                 Token::LeftBracket => self.parse_list_value(),
                 Token::LeftBrace => self.parse_map_value(),
                 _ => Err(ParseError::InvalidValueDeclaration(self.start_pos())),
@@ -396,7 +470,7 @@ impl<'a> Parser<'a> {
     }
 
     fn expect_token(&mut self, expected: Token) -> Result<(), ParseError> {
-        match &self.current_token {
+        match self.token() {
             Some(token) if token == &expected => Ok(()),
             Some(_) => Err(ParseError::UnexpectedToken(self.start_pos())),
             None => Err(ParseError::UnexpectedEOF(self.start_pos())),
@@ -410,7 +484,7 @@ impl<'a> Parser<'a> {
 
     fn skip_comments(&mut self) {
         loop {
-            if let Some(token) = &self.current_token {
+            if let Some(token) = self.token() {
                 if token == &Token::LineComment || token == &Token::BlockComment {
                     self.advance();
                     continue;
